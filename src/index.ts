4309affcb30349602689bcfbea41290cea3f6760
TomasLight <art.ser.ignatiev@gmail.com>
2020-07-11 22:33:09 +0300

    add IValidatorAsync to export from index

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from "./Errors";
 export * from "./IModelState";
 export * from "./IModelStateError";
 export * from "./IValidator";
+export * from "./IValidatorAsync";
 export * from "./ModelState";
 export * from "./ModelStateError";
