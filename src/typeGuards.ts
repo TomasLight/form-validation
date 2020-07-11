c396aa601f2c6f42961fa1b47f2e5f7012525e76
TomasLight <art.ser.ignatiev@gmail.com>
2020-07-11 23:07:29 +0300

    fix export

--- src/typeGuards.ts
+++ src/typeGuards.ts
@@ -1,10 +1,10 @@
 import { IValidator } from "./IValidator";
 import { IValidatorAsync } from "./IValidatorAsync";
 
-function isValidator(validator: any): validator is IValidator<any> {
+export function isValidator(validator: any): validator is IValidator<any> {
     return typeof validator.validate === "function";
 }
 
-function isValidatorAsync(validator: any): validator is IValidatorAsync<any> {
+export function isValidatorAsync(validator: any): validator is IValidatorAsync<any> {
     return typeof validator.validateAsync === "function";
 }
