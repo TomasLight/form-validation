8861d1ff10c54179be3935b47d533eeb9343a288
TomasLight <art.ser.ignatiev@gmail.com>
2020-07-29 22:46:06 +0300

    update Errors type

--- src/Errors.ts
+++ src/Errors.ts
@@ -1,6 +1,8 @@
 import { IModelStateError } from "./IModelStateError";
 
 export class Errors {
+    [property: string]: any;
+
     public addStateError(stateError: IModelStateError) {
         if (!this.containsField(stateError.name)) {
             this.addField(stateError.name, stateError.error);
@@ -8,12 +10,10 @@ export class Errors {
     }
 
     private containsField(fieldName: string): boolean {
-        // @ts-ignore
         return typeof this[fieldName] !== "undefined";
     }
 
     private addField(fieldName: string, value: string | object): void {
-        // @ts-ignore
         this[fieldName] = value;
     }
 }
